3bc92aeaaea3ea17e3e080121d6ba1839a992111
chifek <chifek@gmail.com>
2021-07-17 02:33:34 +0300

    added logs;

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const fs = require('fs')
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -13,8 +13,10 @@ app.get('/', (req, res) => {
 app.use(express.static(__dirname + '/assets'))
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  writeConnectionLogs()
   socket.on('chat message', (data) => {
+    let log = 'User ' + data.userName + ' is sent next message: ' + data.message
+    writeDailyLog(log)
     io.emit('chat message', {
       message: data.message,
       userName: data.userName
@@ -22,6 +24,28 @@ io.on('connection', (socket) => {
   })
 });
 
+function writeDailyLog(content) {
+  let date = new Date()
+  const name = date.getFullYear() + '-' + date.getDay() + '-' + date.getMonth()
+  fs.appendFile('public/logs/' + name + '.txt', content + ' --- at ' + date.toString() + '\n', err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+}
+
+function writeConnectionLogs() {
+  let date = new Date()
+  const name = 'connections-' + date.getFullYear() + '-' + date.getDay() + '-' + date.getMonth()
+  fs.appendFile('public/logs/' + name + '.txt', ' user connected at ' + date.toString() + '\n', err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+}
+
 server.listen(3000, () => {
-  console.log('listening on: 3000');
+  writeDailyLog('the server is started')
 });
